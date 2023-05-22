0e0d78335f1c679dfcb3d250f194c6f8d053bb81
Hugo <hugo.cordoba@factorlibre.com>
2023-05-22 16:44:02 +0200

    [UPD] product_sequence

--- product_sequence/__manifest__.py
+++ product_sequence/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Product Sequence",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "author": "Zikzakmedia SL,Sodexis,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-attribute",
     "license": "AGPL-3",
